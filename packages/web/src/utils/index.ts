40711a44a475e419360a577aef283a2aef7ee089
Alexandr Tokarev <63711180+sssweetie@users.noreply.github.com>
2023-06-11 17:15:14 +0300

    feat: sortByDate

--- packages/web/src/utils/index.ts
+++ packages/web/src/utils/index.ts
@@ -2,3 +2,4 @@ export { generateActivities } from './generateActivities';
 export { getActivityColor } from './getActivityColor';
 export { sortByTime } from './sortByTime';
 export { generateCalendarDays } from './generateCalendarDays';
+export { sortByDate } from './sortByDate';
